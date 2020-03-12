f24ce7603ec93e5e712da3312670642c41a38801
tonyfang <q710370@gmail.com>
2020-03-12 13:26:03 +0800

    fix getTweets function

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -23,7 +23,6 @@ let tweetController = {
       }))
 
       User.findAll({
-        limit: 10,
         include: [
           { model: User, as: 'followerId' }
         ]
@@ -39,7 +38,7 @@ let tweetController = {
           isFollowed: user.followerId.map(d => d.id).includes(req.user.id)
         }))
         // 依追蹤者人數排序清單
-        users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+        users = users.sort((a, b) => b.FollowerCount - a.FollowerCount).slice(0, 10)
         return res.render('tweets', {
           tweets: data,
           users: JSON.parse(JSON.stringify(users)),
